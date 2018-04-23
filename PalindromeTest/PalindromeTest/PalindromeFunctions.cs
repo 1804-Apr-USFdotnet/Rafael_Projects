733ad8cb2d4eac7d25f1f2d0ff2f0e5ec9f6dab7
rafael berger <Rafiberger613@gmail.com>
2018-04-23 11:02:50 -0400

    Initial

---
+++ PalindromeTest/PalindromeTest/PalindromeFunctions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeTest
+{
+    public class PalindromeFunctions
+    {
+        public static    bool isPalindrome(string input)
+        {
+            bool result = false;
+            try
+            {
+                bool isPalindrome =  input == new string(input.Reverse().ToArray());
+
+                result = true;
+            }
+
+            catch
+            {
+
+
+
+            }
+            return result;
+
+        }
+
+
+
+
+
+
+    }
+}
