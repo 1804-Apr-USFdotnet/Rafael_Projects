bb8d9ba147f8c6c080a123e0b8481a02eee46420
Rafael Berger <RafiBerger613@gmail.com>
2018-04-30 14:45:17 -0400

    adding resterantWeb Project

---
+++ ResterauntWeb/ResterauntsSln/ResterauntsWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ResterauntsWeb.Startup))]
+namespace ResterauntsWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
